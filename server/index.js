42a73638e3ce585eef443b6719e7e61f8cfc6f9e
ambsdk92 <ambsdk92@gmail.com>
2023-09-18 09:47:50 +0530

    add db connection

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const app = express();
 console.log("Establishing database connection...");
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.use("/api", server);
 
