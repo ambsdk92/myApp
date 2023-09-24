f3e41c84be9d041dda212ed0c47efe2e00a7dc09
ambsdk92 <ambsdk92@gmail.com>
2023-09-24 19:32:54 +0530

    added base URL with env condition

--- src/constants/url-endpoints.js
+++ src/constants/url-endpoints.js
@@ -1,2 +1,6 @@
-const BASE_URL = "https://myapp-server.cyclic.cloud";
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://myapp-server.cyclic.cloud"
+    : "http://localhost:4000";
+
 export const USERS_URL = `${BASE_URL}/api/users`;
